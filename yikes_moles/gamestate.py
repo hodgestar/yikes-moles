4be32088b27d28df59016e858d497199d9b46ba0
Simon Cross <hodgestar@gmail.com>
2016-09-15 23:00:04 +0200

    Yikes, moles.

--- yikes_moles/gamestate.py
+++ yikes_moles/gamestate.py
@@ -3,4 +3,17 @@
 
 
 class GameState(object):
-    pass
+    def __init__(self, holes=6):
+        self._holes = [None] * holes
+
+    def holes(self):
+        return range(0, len(self._holes))
+
+    def mole_at(self, i):
+        return self._holes[i] is not None
+
+    def add_mole(self, i):
+        self._holes[i] = True
+
+    def remove_mole(self, i):
+        self._holes[i] = None
--- yikes_moles/scenes/moles.py
+++ yikes_moles/scenes/moles.py
@@ -1,17 +1,61 @@
 """ Mole whacking scene. """
 
+import random
+
 import pygame.draw
 import pygame.locals as pgl
 
+from ..constants import SCREEN_SIZE
 from ..events import QuitEvent
 from .base import BaseScene
 
 
+class Hole(object):
+    def __init__(self, pos, radius):
+        self.pos = pos
+        self.radius = radius
+        self.mole = False
+
+    def collides(self, pos):
+        dx, dy = pos[0] - self.pos[0], pos[1] - self.pos[1]
+        return dx ** 2 + dy ** 2 < self.radius ** 2
+
+    def render(self, surface):
+        pygame.draw.circle(
+            surface, (255, 0, 0), self.pos, self.radius, width=4)
+        if self.mole:
+            pygame.draw.circle(
+                surface, (0, 0, 255), self.pos, self.radius - 10)
+
+
 class MolesScene(BaseScene):
+    def enter(self, gamestate):
+        self._holes = []
+        margin = 100
+        holes = gamestate.holes()
+        diameter = (SCREEN_SIZE[0] - 2 * margin) / len(holes)
+        radius = int(diameter / 2.5)
+        for i in holes:
+            pos = (margin + diameter / 2 + i * diameter, 350)
+            self._holes.append((i, Hole(pos, radius)))
+
     def event(self, ev, gamestate):
         if ev.type == pgl.KEYDOWN:
             if ev.key in (pgl.K_q, pgl.K_ESCAPE):
                 QuitEvent.post()
+        elif ev.type == pgl.MOUSEBUTTONDOWN:
+            for i, hole in self._holes:
+                if hole.collides(ev.pos):
+                    gamestate.remove_mole(i)
+                    break
+
+    def tick(self, gamestate):
+        for h in gamestate.holes():
+            if random.random() < 0.01:
+                gamestate.add_mole(h)
 
     def render(self, screen, gamestate):
-        pygame.draw.circle(screen, (255, 0, 0), (300, 400), 40, width=4)
+        screen.fill((0, 0, 0))
+        for i, hole in self._holes:
+            hole.mole = gamestate.mole_at(i)
+            hole.render(screen)
